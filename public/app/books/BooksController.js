c447525ff69311aa72bd4a70daf57c5659582589
Vaibhav Bali <vibsbali@gmail.com>
2016-10-14 09:58:19 +1100

    init

---
+++ public/app/books/BooksController.js
@@ -0,0 +1,14 @@
+(function() {
+
+    angular.module('app')
+        .controller('BooksController', BooksController);
+
+
+    function BooksController() {
+
+        var vm = this;
+
+    }
+
+
+}());
