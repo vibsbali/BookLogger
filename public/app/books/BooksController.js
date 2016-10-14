d87019fcd7b639dc3af4ec9dc56a2d00d520472b
Vaibhav Bali <vibsbali@gmail.com>
2016-10-14 16:06:41 +1100

    Created loggerService using Service

--- public/app/books/BooksController.js
+++ public/app/books/BooksController.js
@@ -3,14 +3,13 @@
     angular.module('app')
         .controller('BooksController', BooksController);
 
-    function BooksController(books, dataService) {
+    function BooksController(books, dataService, logger) {
 
         var vm = this;
 
         vm.appName = books.appName;
         vm.allBooks = dataService.getAllBooks();
-        console.log(vm.allBooks);
-    }
 
-
+        logger.output("BooksController has been created");
+    };
 }());
--- public/app/services/loggerService.js
+++ public/app/services/loggerService.js
@@ -1,3 +1,28 @@
 /**
  * Created by vaibhba on 14/Oct/2016.
  */
+"use strict";
+
+(function(){
+
+    function LoggerBase() {
+
+    }
+
+    LoggerBase.prototype.output = function(message){
+        console.log("LoggerBase: " + message);
+    };
+
+  angular.module("app")
+      .service("logger", BookAppLogger);
+
+    function BookAppLogger(){
+        LoggerBase.call(this);
+
+        this.logBook = function(book){
+            console.log("Book: " + book.title);
+        }
+    };
+
+    BookAppLogger.prototype = Object.create(LoggerBase.prototype);
+}());
